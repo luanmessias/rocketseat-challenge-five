deb51a92c0f856b8ffe6bbdc8dc806a63e10adbd
Luan Messias <luanmessias10@gmail.com>
2020-09-11 22:04:03 +0100

    Added files about rocketseat test five

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    if (title === '') {
+      throw new Error('Title must be provided');
+    }
+
+    if (value === 0) {
+      throw new Error(`Value can't be 0`);
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new Error('Transactions accept only income and outcome options');
+    }
+
+    return transaction;
   }
 }
 
